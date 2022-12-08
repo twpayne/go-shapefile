1a250ad258c48ad8b0737944cee1536b0453937e
Tom Payne <twpayne@gmail.com>
2022-12-09 00:33:56 +0100

    chore: add fuzzing of ReadSHP

---
+++ shp_test.go
@@ -0,0 +1,33 @@
+package shapefile
+
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func FuzzReadSHP(f *testing.F) {
+	require.NoError(f, fs.WalkDir(os.DirFS("."), "testdata", func(path string, dirEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !strings.HasSuffix(path, ".shp") {
+			return nil
+		}
+		data, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		f.Add(data)
+		return nil
+	}))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		r := bytes.NewReader(data)
+		ReadSHP(r, int64(len(data)))
+	})
+}
